7a5fd9c68ebd9fbad725894f800f2dccbb7bc6ce
عبدالحميد عماد معروف ابراهيم <oshaemad1332007@gmail.com>
2026-04-27 03:35:55 +0300

    Add files via upload

--- web.js
+++ web.js
@@ -1,3 +1,7 @@
+// ----------------------------------------------------
+// ----------------------------------------------------
+// slide par
+
 let sidebar = document.getElementById("sidebar");
 
 function toggleSidebar() {
@@ -7,5 +11,30 @@ sidebar.classList.toggle("active");
 
 
 
+
+
+// ---------------------------------------------------------
+// ------------------------------------------------------------
+// change btn
+
+
+let btn2 = document.querySelector(".btn2");
+let cards = document.querySelectorAll(".cardsimg");
+
+
+
+if (localStorage.getItem("mode_change") === "change") {
+cards.forEach(el => el.classList.add("change"));
+}
+
+btn2.onclick = function () {
+cards.forEach(el => el.classList.toggle("change"));
+
+if (cards[0].classList.contains("change")) {
+    localStorage.setItem("mode_change", "change");
+} else {
+    localStorage.setItem("mode_change", "notchange");
+}
+};
 
 
